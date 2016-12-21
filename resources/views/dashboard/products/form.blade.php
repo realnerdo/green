f8cd4bb2a1da9f9cff70b68fa74587d7281ffa45
Asael Chavez <asaelx@gmail.com>
2016-12-21 10:44:59 -0600

    Fix product variable validation for form view

--- resources/views/dashboard/products/form.blade.php
+++ resources/views/dashboard/products/form.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    @if ((isset($product)))
+    @if ($product->id)
         <div class="photo">
             <img src="{{ $product->medias->first()->url }}" alt="" class="img" width="300">
         </div>
@@ -21,27 +21,27 @@
 <!-- /.form-group -->
 <div class="form-group">
     {{ Form::label('sku', 'SKU', ['class' => 'label']) }}
-    {{ Form::input('text', 'sku', (isset($product)) ? $product->variations->first()->meta->sku : null, ['class' => 'input']) }}
+    {{ Form::input('text', 'sku', ($product->id) ? $product->variations->first()->meta->sku : null, ['class' => 'input']) }}
 </div>
 <!-- /.form-group -->
 <div class="form-group">
     {{ Form::label('regular_price', 'Precio regular', ['class' => 'label']) }}
-    {{ Form::input('text', 'regular_price', (isset($product)) ? $product->variations->first()->meta->regular_price : null, ['class' => 'input']) }}
+    {{ Form::input('text', 'regular_price', ($product->id) ? $product->variations->first()->meta->regular_price : null, ['class' => 'input']) }}
 </div>
 <!-- /.form-group -->
 <div class="form-group">
     {{ Form::label('sale_price', 'Precio de oferta', ['class' => 'label']) }}
-    {{ Form::input('text', 'sale_price', (isset($product)) ? $product->variations->first()->meta->sale_price : null, ['class' => 'input']) }}
+    {{ Form::input('text', 'sale_price', ($product->id) ? $product->variations->first()->meta->sale_price : null, ['class' => 'input']) }}
 </div>
 <!-- /.form-group -->
 <div class="form-group">
     {{ Form::label('stock', 'En existencia', ['class' => 'label']) }}
-    {{ Form::input('text', 'stock', (isset($product)) ? $product->variations->first()->meta->stock : null, ['class' => 'input']) }}
+    {{ Form::input('text', 'stock', ($product->id) ? $product->variations->first()->meta->stock : null, ['class' => 'input']) }}
 </div>
 <!-- /.form-group -->
 <div class="form-group">
     {{ Form::label('category', 'Categoría', ['class' => 'label']) }}
-    {{ Form::select('category_list[]', $categories, (isset($product)) ? $product->categories->first()->id : null, ['multiple', 'class' => 'select']) }}
+    {{ Form::select('category_list[]', $categories, ($product->id) ? $product->categories->first()->id : null, ['multiple', 'class' => 'select']) }}
 </div>
 <!-- /.form-group -->
 <div class="form-group">
