d0b4bd54f328e8d74671fed46cb88efe1f3bcae7
Asael Chávez <asaelx@gmail.com>
2017-01-23 01:44:15 -0600

    Change title

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title', 'VentasVerdes')</title>
+        <title>@yield('title', 'Ventas Verdes')</title>
         {{ Html::style('css/app.css') }}
     </head>
     <body>
